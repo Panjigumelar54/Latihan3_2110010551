266d1b9b07eb23a087d3e385171ba45e5f22e401
Panjigumelar54 <134267886+Panjigumelar54@users.noreply.github.com>
2023-06-14 09:36:59 +0800

    Membuat class Main dan Menjalankan program

--- src/geometri/PersegiPanjang.java
+++ src/geometri/PersegiPanjang.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package geometri;
 
-/**
- *
- * @author ASUS
- */
 public class PersegiPanjang {
-    
+     private final double panjang;
+    private final double lebar;
+
+    public PersegiPanjang(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+
+    public double luas() {
+        return panjang * lebar;
+    }
+
+    public double keliling() {
+        return 2 * (panjang + lebar);
+    }
 }
